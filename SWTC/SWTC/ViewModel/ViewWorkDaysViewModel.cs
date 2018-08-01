ce067d5953e5be907a6ee7829c176749c7a69aac
Porkki <porkki123@gmail.com>
2018-08-01 16:09:17 +0300

    Created new VM for listing all workdays

---
+++ SWTC/SWTC/ViewModel/ViewWorkDaysViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SWTC.ViewModel
+{
+    class ViewWorkDaysViewModel : BaseVM
+    {
+        public INavigation Navigation { get; set; }
+        public ViewWorkDaysViewModel(INavigation navigation)
+        {
+            this.Navigation = navigation;
+        }
+    }
+}
