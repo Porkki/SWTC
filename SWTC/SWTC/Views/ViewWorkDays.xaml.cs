ce85ac76641408a1338180b636a2796702f4ebe2
Porkki <porkki123@gmail.com>
2018-08-01 16:05:28 +0300

    Started creating view for seeing workdays

---
+++ SWTC/SWTC/Views/ViewWorkDays.xaml.cs
@@ -0,0 +1,22 @@
+﻿using SWTC.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SWTC.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ModifyWorkDay : ContentPage
+	{
+		public ModifyWorkDay ()
+		{
+			InitializeComponent ();
+            BindingContext = new ViewWorkDaysViewModel(Navigation);
+		}
+	}
+}
